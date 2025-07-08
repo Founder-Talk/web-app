9e18f52a70a908a8d7cb66bdff3f400267d71bbc
YASH2204V <yash.v220104@gmail.com>
2025-07-09 01:46:54 +0530

    checked sessions CRUD

--- backend/routes/session.routes.js
+++ backend/routes/session.routes.js
@@ -17,18 +17,28 @@ const authMiddleware = require("../middlewares/authMiddleware");
 // All session routes require authentication
 router.use(authMiddleware);
 
+// FOR MENTEES 🐦‍🔥🐦‍🔥
+// ----------------------------------
+
 // Session management
 router.post("/", createSession);
 router.get("/", getUserSessions);
 router.get("/:sessionId", getSessionDetails);
+// ------------------------------------------
+
+// -----------------------------------------------------------------
+
+// FOR MENTORS 🐦‍🔥🐦‍🔥
+// ----------------------------------
 
 // Session actions
 router.put("/:sessionId/accept", acceptSession);
 router.put("/:sessionId/reject", rejectSession);
 router.put("/:sessionId/complete", completeSession);
-router.put("/:sessionId/cancel", cancelSession);
+router.put("/:sessionId/cancel", cancelSession); // Should Applicable for both Mentor (after he accept the session) & Mentee.
+// -------------------------------------------
 
 // Feedback
-router.post("/:sessionId/feedback", addSessionFeedback);
+router.post("/:sessionId/feedback", addSessionFeedback); // Feedback by mentees after session completion.
 
 module.exports = router; 
